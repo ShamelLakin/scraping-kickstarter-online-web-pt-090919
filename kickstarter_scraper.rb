352fb20b03eb612c7f5090d833fa78bc8c115aed
Shamel Lakin <christlife4u@gmail.com>
2019-10-17 17:06:57 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -6,6 +6,7 @@ require "pry"
 # projects: kickstarter.css("li.project.grid_4")
 # title: project.css("h2.bbcard_name strong a").text
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
+# percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i 
 
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
